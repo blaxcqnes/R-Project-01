f08be6764a48066d131e9e901436361d180ddd89
Mahmoud Taha <blaxcq@gmail.com>
2025-02-21 21:44:04 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,16 @@ import EntryData from './data/EntryData.jsx';
 function App() {
   let entryElements = EntryData.map((entries) => {
     return (
-      <>
-        <Entry
-          image={entries.image}
-          locSvg={entries.locSvg}
-          countryName={entries.countryName}
-          gmap={entries.gmap}
-          title={entries.title}
-          dates={entries.dates}
-          description={entries.description}
-        />
-      </>
+      <Entry
+        key={entries.id}
+        image={entries.image}
+        locSvg={entries.locSvg}
+        countryName={entries.countryName}
+        gmap={entries.gmap}
+        title={entries.title}
+        dates={entries.dates}
+        description={entries.description}
+      />
     );
   });
 
